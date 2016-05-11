94fe1d62c4697b3b22be1da4cd8dfe4260f13738
Solomon1732 <shlomireu5@gmail.com>
2016-05-11 16:15:01 +0300

    Added Copyright
    Added copyright to all the files, and added short description to the
    project packages.

--- JavaUtils/src/main/java/com/sol/propertyfiles/package-info.java
+++ JavaUtils/src/main/java/com/sol/propertyfiles/package-info.java
@@ -13,30 +13,11 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  *******************************************************************************/
-package com.sol.browser;
-
 /**
- * An enum containing the most common properties
- * @author Shlomi Reuveni
- * @version %I%, %G%
- * @since Dec 29 2015
+ * Contains classes for handling property files.
  */
-public enum BrowserPropertyConstants {
-	USERNAME("username"), PASSWORD("password"), IMPLICITE_WAITING_TIME("implicit-waiting-time"),
-	URL("url"), PATH_TO_DRIVER("path-to-driver");
-
-	private final String value;
-
-	private BrowserPropertyConstants(final String value) {
-		assert null != value : "value is null!";
-		this.value = value;
-	}
-	
-	/**
-	 * Get the string contained in the enum
-	 * @return a string of the value contained within the enum
-	 */
-	public String getPropertyValue() {
-		return value;
-	}
-}
+/**
+ * @author Shlomi Reuveni
+ *
+ */
+package com.sol.propertyfiles;
